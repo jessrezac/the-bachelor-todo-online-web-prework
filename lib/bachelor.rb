8bd4ea1f608b032b653bb922bc3caffe17eb319d
Jess Rezac <jess.rezac@gmail.com>
2019-05-02 16:55:18 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,69 @@
+require 'pry'
+
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winning_contestant = ""
+
+  data.each do |season_label, contestants|
+    if season_label == season
+      contestants.each do |contestant_details|
+        if contestant_details["status"] == "Winner"
+          winning_contestant = contestant_details["name"]
+        end
+      end
+    end
+  end
+
+  winning_contestant = winning_contestant.split[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant_name = ""
+
+  data.each do |season_label, contestants|
+      contestants.each do |contestant_details|
+        if contestant_details["occupation"] == occupation
+          contestant_name = contestant_details["name"]
+        end
+      end
+  end
+
+  contestant_name
+
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  contestant_count = 0
+
+  data.each do |season_label, contestants|
+      contestants.each do |contestant_details|
+        if contestant_details["hometown"] == hometown
+          contestant_count += 1
+        end
+      end
+  end
+
+  contestant_count
+
+
 end
 
 def get_occupation(data, hometown)
   # code here
+  contestant_occupation = ""
+
+  contestant = data.each do |season_label, contestants|
+      contestants.each do |contestant_details|
+        while contestant_occupation == ""
+          if contestant_details["hometown"] == hometown
+            contestant_occupation = contestant_details["occupation"]
+          end
+        else
+          break
+        end
+      end
+  end
+
 end
 
 def get_average_age_for_season(data, season)
